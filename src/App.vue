2ac4df2a51d2757ea90fbd00bb7f6ce8e6139c14
Auvray-ext <Claire.Auvray-ext@sanofi.com>
2020-11-30 13:49:41 +0100

    feat(feedback): generic component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <v-app>
-    <glisten-client />
-      </v-app>
+    <v-btn color="green" dark @click="sheet = !sheet">
+      Add feedback
+    </v-btn>
+    <glisten-client
+      :sheet="sheet"
+      :applicationID="applicationID"
+      :data="data"
+      @close="toggleFeedback"
+    />
+  </v-app>
 </template>
 
 <script lang="ts">
@@ -20,6 +28,14 @@ import { WhispService } from './services/whisp.service';
   },
 })
 export default class App extends Vue {
+  public applicationID = 'GLISTEN';
+  public data = {
+    contextPortal: window.location.href,
+    contextPage: '',
+  };
+
+  public sheet = false;
+
   constructor() {
     super();
     WhispService.httpURL = process.env.VUE_APP_WHISPR_API_HTTP_URL
@@ -28,6 +44,10 @@ export default class App extends Vue {
     WhispService.wsURL = process.env.VUE_APP_WHISPR_API_WS_URL
       ? process.env.VUE_APP_WHISPR_API_WS_URL
       : '';
+  }
+
+  private toggleFeedback(event: any) {
+    this.sheet = false;
   }
 }
 </script>
--- src/components/GlistenClient.vue
+++ src/components/GlistenClient.vue
@@ -1,40 +1,76 @@
 <template>
-  <v-container fluid>
-    <v-text
-      class="title py-3"
-    >Thanks for using App X!!! How likely are you to recommend it to your colleagues?</v-text>
+  <div class="text-center">
+    <v-bottom-sheet v-model="show" max-width="70%">
+      <v-sheet class="text-center sheet--feedback">
+        <v-container fluid>
+          <div class="title py-3">
+            Thanks for letting us know how do you like this and sending your feedback to further
+            improve.
+          </div>
 
-    <v-rating
-      v-model="glistenWhisp.data.rating"
-      color="red"
-      background-color="grey darken-1"
-      half-increments
-      hover
-      x-large
-      empty-icon="mdi-heart-outline"
-      full-icon="mdi-heart"
-      half-icon="mdi-heart-half-full"
-    ></v-rating>
-    <v-textarea
-      v-model="glistenWhisp.data.comment"
-      outlined
-      name="input-7-4"
-      label="Leave us a comment"
-      auto-grow
-      rows="2"
-    ></v-textarea>
+          <v-rating
+            v-model="glistenWhisp.data.rating"
+            color="red"
+            background-color="grey darken-1"
+            half-increments
+            hover
+            clearable
+            x-large
+            empty-icon="mdi-heart-outline"
+            full-icon="mdi-heart"
+            half-icon="mdi-heart-half-full"
+          ></v-rating>
+          <v-textarea
+            v-model="glistenWhisp.data.feedback"
+            outlined
+            name="input-7-4"
+            label="Leave us a comment"
+            auto-grow
+            rows="2"
+          ></v-textarea>
+          <v-row no-gutters row wrap>
+            <v-col xs12 sm6 md4>
+              <v-switch
+                v-model="glistenWhisp.data.anonymous"
+                class="my-0 py-0 px-1"
+                label="Make my feedback anonymous"
+              ></v-switch>
+              <v-switch
+                v-model="actionRequired"
+                class="my-0 py-0 px-1"
+                label="Action expected"
+              ></v-switch>
+            </v-col>
 
-    <v-switch
-      v-model="glistenWhisp.data.anonymous"
-      class="my-0 py-0 px-1"
-      label="Make my feedback anonymous"
-    ></v-switch>
+            <v-col xs12 sm6 md4>
+              <v-text-field
+                v-if="glistenWhisp.data.anonymous === false"
+                v-model="glistenWhisp.data.name"
+                outlined
+                label="Your name"
+              ></v-text-field>
+            </v-col>
+          </v-row>
 
-    <v-btn color="primary" @click="submitFeedback">Submit feedback</v-btn>
-  </v-container>
+          <v-btn color="primary" :disabled="!glistenWhisp.data.feedback" @click="submitFeedback"
+            >Submit feedback</v-btn
+          >
+        </v-container>
+      </v-sheet>
+    </v-bottom-sheet>
+
+    <v-snackbar v-model="snackbar" :timeout="timeout" color="success" outlined class="pa-10">
+      <p>{{ text }}</p>
+      <template v-slot:action="{ attrs }">
+        <v-btn color="blue" text v-bind="attrs" @click="snackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
 </template>
 
-<script lang='ts'>
+<script lang="ts">
 import { WhispService } from '../services/whisp.service';
 import { Component, Prop, Inject, Provide, Vue, Watch } from 'vue-property-decorator';
 
@@ -42,36 +78,100 @@ import { Component, Prop, Inject, Provide, Vue, Watch } from 'vue-property-decor
   components: {},
 })
 export default class GlistenClient extends Vue {
-  @Provide() public user = 'user not set';
-  @Provide() public surveyQuestion = '';
-  private data!: any;
+  @Prop()
+  public sheet!: boolean;
+
+  @Prop()
+  public data!: any;
+
+  @Prop()
+  public applicationID!: string;
+
+  get show() {
+    return this.sheet;
+  }
+
+  set show(val) {
+    this.$emit('close', val);
+  }
+
+  private actionRequired = false;
+  private snackbar = false;
+  private timeout = 3000;
+  private text = '';
+
   private glistenWhisp = {
-    type: 'USER_FEEDBACK',
-    applicationID: 'GLISTEN',
-    openedById: '',
+    type: 'GLISTEN',
+    applicationID: this.applicationID,
+    openedBy: '',
+    description: '',
     data: {
       anonymous: false,
-      comment: '',
-      rating: 0,
+      feedback: null,
+      rating: 4.5,
+      name: '',
+      closed: null,
       commentSentimentScore: 0,
     },
+    timestamp: '', // = new Date(Date.now()) '2020-10-22T20:49:06.842Z'
   };
 
   private newWhisp: any = '';
 
-  constructor() {
-    super();
-    this.data = [];
-  }
-
   @Watch('glistenWhisp.data.anonymous', { immediate: true })
   private onAnonymousChanged(val: boolean, oldVal: boolean) {
-    this.glistenWhisp.openedById = val ? '' : this.user;
+    this.glistenWhisp.data.name = val ? '' : this.glistenWhisp.data.name;
+  }
+
+  @Watch('actionRequired', { immediate: true })
+  private onActionChanged(val: boolean, oldVal: boolean) {
+    this.glistenWhisp.data.closed = val ? false : (null as any);
+  }
+
+  private calculateSentiment(comment: string): number {
+    const sentiment = require('wink-sentiment');
+    const sentimentResult = sentiment(comment);
+    return sentimentResult.normalizedScore;
+  }
+
+  private truncateDescription(str: string) {
+    const n = 200;
+    if (str.length <= n) {
+      return str;
+    }
+    const subString = str.substr(0, n - 1); // the original check
+    return subString.substr(0, subString.lastIndexOf(' ')) + '...';
   }
 
   private async submitFeedback() {
-    // this.sentimentScore = calculateSentiment(this.comment);
+    this.glistenWhisp.data.commentSentimentScore = this.calculateSentiment(
+      this.glistenWhisp.data.feedback!,
+    );
+
+    this.glistenWhisp.openedBy = this.glistenWhisp.data.name;
+
+    const timestamp = new Date(Date.now());
+    this.glistenWhisp.timestamp = timestamp.toISOString();
+    this.glistenWhisp.description = this.truncateDescription(this.glistenWhisp.data.feedback!);
+
+    this.glistenWhisp.data = { ...this.glistenWhisp.data, ...this.data };
+
+    console.log(this.glistenWhisp);
     this.newWhisp = await WhispService.createWhisp(this.glistenWhisp);
+
+    if (this.glistenWhisp.data) {
+      const name = this.glistenWhisp.openedBy ? this.glistenWhisp.openedBy : '';
+      this.text = `Thanks for your feedback ${name} !`;
+      this.snackbar = true;
+    }
+
+    this.$emit('close', this.glistenWhisp);
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.sheet--feedback {
+  border-radius: 15px 15px 0px 0px;
+}
+</style>
